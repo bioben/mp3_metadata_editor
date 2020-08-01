4a176a1a3c4f9d0ad3a8b0bf3f187dcbb6795d39
bioben <benjaminahn@gatech.edu>
2020-08-01 19:24:23 -0400

    working changeArtist

--- changeArtist.py
+++ changeArtist.py
@@ -1,10 +1,41 @@
-import eyed3
+import eyed3, os
 from eyed3 import id3
 
-#find directory of mp3 files 
+#introductory and summary of script 
+print("*************************************")
+print("Running changeArtist script! \n This will change the metadata by setting an artist name")
+print("*************************************")
+
+print();
+
+#step 1: gather user input
 directory = input("Enter mp3 directory: ")
 print(directory)
+artistName = input("Enter artist name: ")
+print(artistName)
 
-tag = id3.Tag()
-tag.parse("/home/benjamin/Music/AFI - Miss MurderLyrics.mp3")
-print(tag.artist)
+print();
+
+#step 2: run eyeD3 
+mp3List = os.listdir(directory)
+for song in mp3List:
+	#initiate eyeD3 functionality 
+	tag = id3.Tag()
+
+	#find song path 
+	songPath = directory + "/" + song
+	print("running")
+	print(songPath)
+
+	#open song 
+	tag.parse(songPath)
+	
+	#change artist metadata
+	tag.artist = artistName
+
+	#save song file 
+	tag.save()
+
+print()
+print('*************************************')
+print('DONE')
